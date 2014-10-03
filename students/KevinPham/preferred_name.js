10040043eb026359589016ee11745ec73130fc05
Kevin Pham <kevin.v.pham@hotmail.com>
2014-10-02 17:09:13 -0700

    preferred_name.js - new working code w/ JSFIDDLE link

--- students/KevinPham/preferred_name.js
+++ students/KevinPham/preferred_name.js
@@ -1,12 +1,15 @@
-function preferredName (firstName, lastName) {
-	if (!firstName && !lastname || firstName && lastName) {
-		return false;
-	// if (not firstName and not lastName or firstName and lastName), return false
-	} else if (!firstName) {
-		return lastName;
-	// if (not firstName), return lastName
-	} else {
-		return firstName;
-	// else, return firstName
-	}
-};
+var firstName, lastName;
+
+function preferredName(firstName, lastName) {
+    if (!firstName && !lastName) {
+        return "False: What is your preferred name?";
+    } else if (firstName && lastName) {
+        return "False: Which name do you want to be called by?";
+    } else if (!firstName) {
+        return lastName;
+    } else {
+        return firstName;
+    }
+}
+
+// JSFIDDLE: http://jsfiddle.net/kpham13/mengu48r/4/
